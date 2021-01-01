59b168cef7823af95a60855ecb464addc711bfa0
benza613 <benito.alvares@gmail.com>
2021-01-01 14:24:50 -0600

    daily challenge jan 1st

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Practicing leetcode problems
 |**564**| [Best Time to Buy and Sell Stock II](https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/564/) | [C#](csharp-solutions/top-interview-questions/564_buy_sell_stock.cs) | Easy | 
 |**646**| [Rotate Array](https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/) | [C#](csharp-solutions/top-interview-questions/646_rotate_array.cs) | Easy | 
 |**3586**| [Game of Life](https://leetcode.com/explore/challenge/card/december-leetcoding-challenge/573/week-5-december-29th-december-31st/3586/)| [javascript](monthly-challenges/december/game-of-life.js) | Medium |
+|**3589**| [Check Array Formation Through Concatenation](https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/579/week-1-january-1st-january-7th/3589/)| [python3](monthly-challenges/jan21/array_formation_concat.py) | Easy |
 |||||
 
 
---
+++ monthly-challenges/jan21/array_formation_concat.py
@@ -0,0 +1,40 @@
+from typing import List
+
+
+class Solution:
+    def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
+
+        idx = 0
+
+        while idx < len(arr):
+            el = arr[idx]
+            el_found = False
+            pc_size = 0
+
+            for piece in pieces:
+
+                if set([el]).issubset(set(piece)):
+                    el_found = True
+
+                    if piece != arr[idx : len(piece) + idx]:
+                        return False
+                    else:
+                        pc_size = len(piece)
+                        #print("\t el", el)
+                        break
+
+            if el_found == False:
+                return False
+
+            idx += pc_size
+
+        return True
+
+
+x = Solution()
+
+print(x.canFormArray([85], [[85]]))
+print(x.canFormArray([15, 88], [[88], [15]]))
+print(x.canFormArray([49, 18, 16], [[16, 18, 49]]))
+print(x.canFormArray([91, 4, 64, 78], [[78], [4, 64], [91]]))
+print(x.canFormArray([1, 3, 5, 7], [[2, 4, 6, 8]]))
