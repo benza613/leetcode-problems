18efa0d4df50d46fab4091991ad4a57496b2fd70
BENITO ALVARES <benito.alvares@gmail.com>
2022-03-03 23:30:42 -0600

    Time: 222 ms (11.92%), Space: 15.5 MB (74.24%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        unique_list = [] 
+
+        for x in nums: 
+            if x not in unique_list: 
+                unique_list.append(x)
+                
+        for idx, x in enumerate(unique_list):
+            print(idx,len(unique_list) )
+            if unique_list[idx] == nums[idx]:
+                continue;
+            
+            swap_idx = nums.index(unique_list[idx])
+            nums[idx] = nums[swap_idx]
+            
+        del nums[len(unique_list):]
