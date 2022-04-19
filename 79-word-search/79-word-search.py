c1b02fa5f4e198439b9f39ef5e5f8861f1006e73
BENITO ALVARES <benito.alvares@gmail.com>
2022-04-18 22:12:42 -0500

    Time: 5856 ms (79.20%), Space: 14 MB (53.90%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -15,7 +15,7 @@ class Solution:
 
         for row in range(self.ROWS):
             for col in range(self.COLS):
-                if self.backtrack(row, col, word):
+                if self.backtrack2(row, col, word):
                     return True
 
         # no match found after all exploration
@@ -47,5 +47,33 @@ class Solution:
         # Tried all directions, and did not find any match
         return ret
             
+        
+    def backtrack2(self, row, col, suffix):
+        """
+            backtracking with side-effect,
+               the matched letter in the board would be marked with "#".
+        """
+        # bottom case: we find match for each letter in the word
+        if len(suffix) == 0:
+            return True
+
+        # Check the current status, before jumping into backtracking
+        if row < 0 or row == self.ROWS or col < 0 or col == self.COLS \
+                or self.board[row][col] != suffix[0]:
+            return False
+
+        # mark the choice before exploring further.
+        self.board[row][col] = '#'
+        # explore the 4 neighbor directions
+        for rowOffset, colOffset in [(0, 1), (-1, 0), (0, -1), (1, 0)]:
+            # sudden-death return, no cleanup.
+            if self.backtrack(row + rowOffset, col + colOffset, suffix[1:]):
+                return True
+
+        # revert the marking
+        self.board[row][col] = suffix[0]
+
+        # Tried all directions, and did not find any match
+        return False
             
                 
